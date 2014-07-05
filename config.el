7d62cc2028645da8ab465536d3baa9f042e4232d
expormz <yanxin@MyMacBook-Pro.local>
2014-07-05 23:13:22 +0800

    added el-get,ace-jump,evil,evil-leader

--- config.el
+++ config.el
@@ -1,8 +1,10 @@
 ;;theme config
-(add-to-list 'load-path "~/.emacs.d/lib/color-theme")
-(require 'color-theme)
-(load-file "~/.emacs.d/lib/cyberpunk.el")
-(color-theme-cyberpunk)
+;;(add-to-list 'load-path "~/.emacs.d/lib/color-theme")
+;;(require 'color-theme)
+;;(load-file "~/.emacs.d/lib/cyberpunk.el")
+;;(color-theme-cyberpunk)
+(require 'moe-theme)
+(moe-dark)
 ;;font config
 (when (display-graphic-p)
   (setq fonts
@@ -21,6 +23,9 @@
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+;;el-get
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+(require 'el-get)
 ;;base config
 (global-linum-mode 1)
 (column-number-mode 1)
@@ -53,8 +58,16 @@
 ;;disable CJK coding/encoding (Chinese/Japanese/Korean characters)
 (setq utf-translate-cjk-mode nil)
 ;;powerline
-(add-to-list 'load-path "~/.emacs.d/lib/powerline")
-(require 'powerline)
+;;(add-to-list 'load-path "~/.emacs.d/lib/powerline")
+;;(require 'powerline)
+
+;;ace-jump-mode
+(add-to-list 'load-path "~/.emacs.d/lib/ace-jump-mode")
+(autoload 'ace-jump-mode "ace-jump-mode" "Emacs quick move minor mode" t)
+(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+(autoload 'ace-jump-mode-pop-mark "ace-jump-mode" "ace jump back" t)
+(eval-after-load "ace-jump-mode" '(ace-jump-mode-enable-mark-sync))
+(define-key global-map (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
 ;;heml mode
 (add-to-list 'load-path "~/.emacs.d/lib/helm")
 (require 'helm-config)
@@ -86,6 +99,18 @@
 (require 'yasnippet)
 (setq yas-snippet-dirs '("~/.emacs.d/lib/yasnippet/snippets" "~/.emacs.d/lib/yasnippet/yasmate/snippets"))
 (yas-global-mode 1)
+;;evil-mode
+(require 'evil)
+(require 'evil-leader)
+(evil-mode 1)
+(global-evil-leader-mode)
+(evil-leader/set-leader ",")
+(evil-leader/set-key
+  "e" 'find-file
+  "b" 'switch-to-buffer
+  "k" 'kill-buffer
+  "w" 'save-buffer
+  "q" 'quit)
 ;;auto-complete
 (add-to-list 'load-path "~/.emacs.d/lib")
 (require 'go-autocomplete)
@@ -121,25 +146,28 @@
 
 ;;popwin
 (require 'popwin)
-(popwin-mode 1) 
+(popwin-mode 1)
+;;undo tree mode
+(require 'undo-tree)
+(global-undo-tree-mode)
 ;;js2 mode
 (add-to-list 'load-path "~/.emacs.d/lib/js2")
 (require 'js2-mode)
 ;;go-mode
 (require 'go-mode-load)
 ;;path config
-;;(defun my-add-path (path-element)
-;;  "Add the specified PATH-ELEMENT to the Emacs PATH."
-;;  (interactive "DEnter directory to be added to path: ")
-;;  (if (file-directory-p path-element)
-;;     (progn
-;;       (setenv "PATH" (concat (expand-file-name path-element) path-separator (getenv "PATH")))
-;;       (add-to-list 'exec-path (expand-file-name path-element)))))
+(defun my-add-path (path-element)
+  "Add the specified PATH-ELEMENT to the Emacs PATH."
+  (interactive "DEnter directory to be added to path: ")
+  (if (file-directory-p path-element)
+     (progn
+       (setenv "PATH" (concat (expand-file-name path-element) path-separator (getenv "PATH")))
+       (add-to-list 'exec-path (expand-file-name path-element)))))
 
-;;(if (fboundp 'my-add-path)
-;;   (let ((my-paths (list "/bin" "/usr/local/go/bin" "/sbin" "/usr/bin" "/usr/local/bin" "/usr/local/git/bin")))
-;;      (dolist (path-to-add my-paths (getenv "PATH"))
-;;         (my-add-path path-to-add))))
+(if (fboundp 'my-add-path)
+   (let ((my-paths (list "/bin" "/usr/local/go/bin" "/sbin" "/usr/bin" "/usr/local/bin" "/usr/local/git/bin")))
+      (dolist (path-to-add my-paths (getenv "PATH"))
+         (my-add-path path-to-add))))
 ;;mode-hook
 (add-hook 'html-mode-hook 'emmet-mode)
 (add-hook 'sgml-mode-hook 'emmet-mode)
@@ -150,9 +178,18 @@
 (global-set-key (kbd "C-c l") 'helm-locate)
 (global-set-key (kbd "C-c i") 'helm-imenu)
 (global-set-key (kbd "C-,") 'set-mark-command)
-;;scroll other window
+(global-set-key (kbd "C-c f") 'helm-for-files)
+(global-set-key (kbd "C-c o") 'ffap-menu)
+(global-set-key (kbd "C-b") 'switch-to-buffer)
+
+				
+;;other window global
 (global-set-key (kbd "C-M-]") 'scroll-other-window)
 (global-set-key (kbd "C-M-[") 'scroll-other-window-down)
+(global-set-key (kbd "M-1") 'delete-other-windows)
+(global-set-key (kbd "M-2") 'split-window-below)
+(global-set-key (kbd "M-3") 'split-window-right)
+(global-set-key (kbd "M-0") 'delete-window)
 
 ;;auto-modes
 (setq auto-mode-alist (cons '("\\.ejs$" . html-mode) auto-mode-alist))
